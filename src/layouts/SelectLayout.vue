66620ec4b087f9fa976a7ae04d00585b42613394
Harper Reed <harper@nata2.org>
2022-01-15 12:56:16 -0600

    prettier'd it up

--- src/layouts/SelectLayout.vue
+++ src/layouts/SelectLayout.vue
@@ -20,7 +20,7 @@ export default {
   data: function () {
     return {
       layout_list: [
-        { route: "Touchscreen", name: "Touchscreen" },
+        { route: "Controller", name: "Controller" },
       ],
     };
   },
--- src/router/index.js
+++ src/router/index.js
@@ -4,30 +4,26 @@ Vue.use(VueRouter);
 
 const router = new VueRouter({
   routes: [
+
     {
       path: "/",
-      name: "SelectLayout",
-      component: () => import("@/layouts/SelectLayout.vue"),
-    },
-    {
-      path: "/touchscreen",
-      name: "Touchscreen",
-      component: () => import("@/layouts/Touchscreen.vue"),
+      name: "Controller",
+      component: () => import("@/layouts/Controller.vue"),
       children: [
         {
           path: "library",
-          name: "TouchscreenLibrary",
-          component: () => import("@/views/TouchscreenLibrary.vue"),
+          name: "Library",
+          component: () => import("@/views/Library.vue"),
         },
         {
           path: "now_playing",
-          name: "TouchscreenNowPlaying",
-          component: () => import("@/views/TouchscreenNowPlaying.vue"),
+          name: "NowPlaying",
+          component: () => import("@/views/NowPlaying.vue"),
         },
         {
           path: "queue",
-          name: "TouchscreenQueue",
-          component: () => import("@/views/TouchscreenQueue.vue"),
+          name: "Queue",
+          component: () => import("@/views/Queue.vue"),
         },
       ],
     },
